17d1c2e65c66b45910768e032b02da55cc0bdfc4
Dmitrii Celpan <dmitrii.celpan@ellation.com>
2018-04-15 13:30:26 +0300

    Add framework project

---
+++ sample-ci-project/sample_ci_project.h
@@ -0,0 +1,19 @@
+//
+//  sample_ci_project.h
+//  sample-ci-project
+//
+//  Created by Dmitrii Celpan on 4/15/18.
+//  Copyright © 2018 lexorus. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for sample_ci_project.
+FOUNDATION_EXPORT double sample_ci_projectVersionNumber;
+
+//! Project version string for sample_ci_project.
+FOUNDATION_EXPORT const unsigned char sample_ci_projectVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <sample_ci_project/PublicHeader.h>
+
+
