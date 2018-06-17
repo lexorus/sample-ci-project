0af27bbd90f9a62b581caf59e14d96c2ddfb22dc
Dmitrii Celpan <celpand@gmail.com>
2018-06-18 01:57:19 +0300

    Make some changes

--- sample-ci-project/SomeFile.swift
+++ sample-ci-project/SomeFile.swift
@@ -3,9 +3,10 @@
 ///
 /// - Parameter snakecasedString: String in the snakecase format
 /// - Returns: String in the lower camelcase format
-func lowerCamelcasedString(from snakecasedString: String) -> String {
+func lowerCamelcasedString(from snakecasedString: String) -> String
+{
     return snakecasedString
-        .components(separatedBy: "_")
+        .components(separatedBy: "_ ")
         .enumerated()
         .map { $0.offset == 0 ? $0.element : $0.element.capitalized }
         .joined()
